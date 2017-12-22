862f10e1709c01981b2e50d1eef07508ccb23e34
Vijay Kumar NM <vijay.nm@apple.com>
2017-12-22 15:53:54 +0530

    Code improvements

--- navUpdate.sh
+++ navUpdate.sh
@@ -1,1 +1,1 @@
-java -DfileName=MutualFunds.xlsx -jar target/NavDownloader-1.0-jar-with-dependencies.jar 118564 119783 119620 103164 119018 118955 105628 119060 118989 120564 119528 119581 119212 120847 119556
+java -DfileName=MutualFunds.xlsx -DnavUrl=https://www.amfiindia.com/spages/NAVAll.txt -jar target/NavDownloader-1.0-jar-with-dependencies.jar 118564 119783 119620 103164 119018 118955 105628 119060 118989 120564 119528 119581 119212 120847 119556
--- src/main/java/com/vijay/mf/nav/App.java
+++ src/main/java/com/vijay/mf/nav/App.java
@@ -17,16 +17,11 @@ import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFDataFormat;
 import org.apache.poi.ss.usermodel.BorderStyle;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.CreationHelper;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFCellStyle;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openxmlformats.schemas.spreadsheetml.x2006.main.impl.CTRowImpl;
 
 public class App {
 
@@ -76,6 +71,8 @@ public class App {
 				workbook.write(fileOut);
 			}
 			workbook.close();
+			System.out.println("\n");
+			System.out.println("Completed Loading the data");
 		}
 	}
 
@@ -88,105 +85,14 @@ public class App {
 		return style;
 	}
 
-	public static void main1(String[] args) throws IOException {
-		try (XSSFWorkbook wb = new XSSFWorkbook()) {
-
-			XSSFSheet sheet = wb.createSheet();
-			XSSFRow row = sheet.createRow(2);
-			row.setHeightInPoints(30);
-			for (int i = 0; i < 8; i++) {
-				// column width is set in units of 1/256th of a character width
-				sheet.setColumnWidth(i, 256 * 15);
-			}
-
-			createCell(wb, row, 0, HorizontalAlignment.CENTER, VerticalAlignment.BOTTOM);
-			createCell(wb, row, 1, HorizontalAlignment.CENTER_SELECTION, VerticalAlignment.BOTTOM);
-			createCell(wb, row, 2, HorizontalAlignment.FILL, VerticalAlignment.CENTER);
-			createCell(wb, row, 3, HorizontalAlignment.GENERAL, VerticalAlignment.CENTER);
-			createCell(wb, row, 4, HorizontalAlignment.JUSTIFY, VerticalAlignment.JUSTIFY);
-			createCell(wb, row, 5, HorizontalAlignment.LEFT, VerticalAlignment.TOP);
-			createCell(wb, row, 6, HorizontalAlignment.RIGHT, VerticalAlignment.TOP);
-
-			// center text over B4, C4, D4
-			row = sheet.createRow(3);
-			centerAcrossSelection(wb, row, 1, 3, VerticalAlignment.CENTER);
-
-			// Write the output to a file
-			try (OutputStream fileOut = new FileOutputStream("xssf-align.xlsx")) {
-				wb.write(fileOut);
-			}
-		}
-	}
-
-	/**
-	 * Center a text over multiple columns using ALIGN_CENTER_SELECTION
-	 *
-	 * @param wb
-	 *            the workbook
-	 * @param row
-	 *            the row to create the cell in
-	 * @param start_column
-	 *            the column number to create the cell in and where the
-	 *            selection starts
-	 * @param end_column
-	 *            the column number where the selection ends
-	 * @param valign
-	 *            the horizontal alignment for the cell.
-	 */
-	private static void centerAcrossSelection(XSSFWorkbook wb, XSSFRow row, int start_column, int end_column,
-			VerticalAlignment valign) {
-		CreationHelper ch = wb.getCreationHelper();
-
-		// Create cell style with ALIGN_CENTER_SELECTION
-		XSSFCellStyle cellStyle = wb.createCellStyle();
-		cellStyle.setAlignment(HorizontalAlignment.CENTER_SELECTION);
-		cellStyle.setVerticalAlignment(valign);
-
-		// Create cells over the selected area
-		for (int i = start_column; i <= end_column; i++) {
-			XSSFCell cell = row.createCell(i);
-			cell.setCellStyle(cellStyle);
-		}
-
-		// Set value to the first cell
-		XSSFCell cell = row.getCell(start_column);
-		cell.setCellValue(ch.createRichTextString("Align It"));
-
-		// Make the selection
-		CTRowImpl ctRow = (CTRowImpl) row.getCTRow();
-
-		// Add object with format start_coll:end_coll. For example 1:3 will span
-		// from
-		// cell 1 to cell 3, where the column index starts with 0
-		//
-		// You can add multiple spans for one row
-		Object span = start_column + ":" + end_column;
-
-		List<Object> spanList = new ArrayList<>();
-		spanList.add(span);
-
-		// add spns to the row
-		ctRow.setSpans(spanList);
-	}
-
-	private static void createCell(XSSFWorkbook wb, XSSFRow row, int column, HorizontalAlignment halign,
-			VerticalAlignment valign) {
-		CreationHelper ch = wb.getCreationHelper();
-		XSSFCell cell = row.createCell(column);
-		cell.setCellValue(ch.createRichTextString("Align It"));
-		CellStyle cellStyle = wb.createCellStyle();
-		cellStyle.setAlignment(halign);
-		cellStyle.setVerticalAlignment(valign);
-		cell.setCellStyle(cellStyle);
-	}
-
 	private static List<MutualFund> getMutualFundList(String[] schemeCodes) {
 		Map<String, MutualFund> mfMap = new HashMap<>(schemeCodes.length);
 		List<MutualFund> mfList = new ArrayList<>(schemeCodes.length);
 
 		try {
-			URL website = new URL("https://www.amfiindia.com/spages/NAVAll.txt");
-			System.out.println("Connecting to URL");
+			String amfiNavURL = System.getProperty("navUrl");
+			URL website = new URL(amfiNavURL);
+			System.out.println("Connecting to URL: " + amfiNavURL);
 			BufferedReader reader = new BufferedReader(new InputStreamReader(website.openStream()));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
@@ -200,15 +106,15 @@ public class App {
 					mFund.setSchemeName(tokens[3]);
 					mFund.setNav(tokens[4]);
 					mFund.setDate(tokens[7]);
-					System.out.println(mFund);
 					mfMap.put(tokens[0], mFund);
 				}
 			}
-			
+
 			for (String schemeCode : schemeCodes) {
 				mfList.add(mfMap.get(schemeCode));
+				System.out.println(mfMap.get(schemeCode));
 			}
-			
+
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
